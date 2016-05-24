a2ff3f991c882f016dea401efe7011c9e8985431
kaidix <xkd0413@gmail.com>
2016-05-24 09:24:55 -0700

    Update notes.sh

--- notes.sh
+++ notes.sh
@@ -1,8 +1,8 @@
 docker-machine create -d virtualbox mh-keystore
 eval "$(docker-machine env mh-keystore)"
 docker run -d     -p "8500:8500"     -h "consul"     progrium/consul -server -bootstrap
-docker-machine create -d virtualbox --swarm --swarm-master --swarm-discovery="consul://$(docker-machine ip mh-keystore):8500" --engine-opt="cluster-store=consul://$(docker-machine ip mh-keystore):8500" --engine-opt="cluster-advertise=eth1:2376" mhs-demo0
-docker-machine create -d virtualbox --swarm --swarm-master --swarm-discovery="consul://$(docker-machine ip mh-keystore):8500" --engine-opt="cluster-store=consul://$(docker-machine ip mh-keystore):8500" --engine-opt="cluster-advertise=eth1:2376" mhs-demo1
+docker-machine create -d virtualbox --virtualbox-memory 4096 --virtualbox-cpu-count 2 --swarm --swarm-master --swarm-discovery="consul://$(docker-machine ip mh-keystore):8500" --engine-opt="cluster-store=consul://$(docker-machine ip mh-keystore):8500" --engine-opt="cluster-advertise=eth1:2376" mhs-demo0
+docker-machine create -d virtualbox --virtualbox-memory 4096 --virtualbox-cpu-count 2 --swarm --swarm-master --swarm-discovery="consul://$(docker-machine ip mh-keystore):8500" --engine-opt="cluster-store=consul://$(docker-machine ip mh-keystore):8500" --engine-opt="cluster-advertise=eth1:2376" mhs-demo1
 eval $(docker-machine env mhs-demo0)
 docker network create --driver overlay --subnet=10.0.9.0/24 my-net
 
